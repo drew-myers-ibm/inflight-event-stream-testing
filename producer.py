7027f076434aed4c264037561b7f3cfe66c99dd3
Drew Myers <acmyers@us.ibm.com>
2021-10-14 14:39:13 -0400

    changed producer to feed in messages fetched from REST API

--- producer.py
+++ producer.py
@@ -1,6 +1,10 @@
 from kafka import KafkaProducer
+import requests
 import json
 import os
+
+client_id = os.environ.get('IF_API_KEY_ID')
+client_secret = os.environ.get('IF_API_KEY_SECRET')
 
 username = os.environ.get('IF_EVENT_USERNAME')
 password = os.environ.get('IF_EVENT_PASSWORD')
@@ -9,11 +13,15 @@ topic_name = os.environ.get('IF_EVENT_TOPIC_NAME')
 
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers, security_protocol='SASL_SSL', sasl_mechanism='PLAIN', sasl_plain_username=username, sasl_plain_password=password)
 
+url = 'https://api.ibm.com/inflight/bluerun/geos/na/environments/cte/documents?sort=-received_at&limit=10'
 
-directory = r'data'
-for filename in os.listdir(directory):
-    with open(os.path.join(directory, filename), 'rb') as reader:
-        inputJson = json.loads(reader.read())
-        producer.send(topic_name, bytes(json.dumps(inputJson, separators=(",", "=")), 'utf-8'))
+headers = {'X-IBM-Client-Id': client_id, 'X-IBM-Client-Secret': client_secret}
+
+r = requests.get(url, headers=headers)
+inner_url = 'https://api.ibm.com/inflight/bluerun/geos/na/environments/cte/documents/'
+for doc in r.json():
+    doc_code = doc['documentCode']
+    inner_r = requests.get(inner_url + doc_code, headers=headers)
+    producer.send(topic_name, bytes(json.dumps(inner_r.json(), separators=(",", "=")), 'utf-8'))
 
 producer.flush()
