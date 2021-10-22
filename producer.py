b1f57dac1501f6cb24709639a0ab00d7faa1db77
Drew Myers <acmyers@us.ibm.com>
2021-10-22 15:17:32 -0400

    added fields for geo, env and companyId

--- producer.py
+++ producer.py
@@ -4,6 +4,11 @@ import json
 import os
 import sys
 
+geo = 'na'
+environment = 'cte'
+customer = 'scbn-trial-buyer'
+base_url = 'https://api.ibm.com/inflight/bluerun/geos/' + geo + '/environments/' + environment
+
 client_id = os.environ.get('IF_API_KEY_ID')
 client_secret = os.environ.get('IF_API_KEY_SECRET')
 
@@ -20,15 +25,19 @@ else:
 
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers, security_protocol='SASL_SSL', sasl_mechanism='PLAIN', sasl_plain_username=username, sasl_plain_password=password)
 
-url = 'https://api.ibm.com/inflight/bluerun/geos/na/environments/cte/documents?sort=-received_at&limit=' + str(limit)
+url = base_url + '/documents?sort=-received_at&limit=' + str(limit)
 
 headers = {'X-IBM-Client-Id': client_id, 'X-IBM-Client-Secret': client_secret}
 
 r = requests.get(url, headers=headers)
-inner_url = 'https://api.ibm.com/inflight/bluerun/geos/na/environments/cte/documents/'
+inner_url = base_url + '/documents/'
 for doc in r.json():
     doc_code = doc['documentCode']
     inner_r = requests.get(inner_url + doc_code, headers=headers)
-    producer.send(topic_name, bytes(json.dumps(inner_r.json(), separators=(",", ":")), 'utf-8'))
+    detailed_doc = inner_r.json()
+    detailed_doc['geo'] = geo
+    detailed_doc['environment'] = environment
+    detailed_doc['companyId'] = customer
+    producer.send(topic_name, bytes(json.dumps(detailed_doc, separators=(",", ":")), 'utf-8'))
 
 producer.flush()
